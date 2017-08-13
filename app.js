26cdfcf66d9bcadc69f73baf36567d0933f0d0b2
venkat <bvenkatr2@gmail.com>
2017-08-13 16:04:25 +0530

    done ROUTING

--- app.js
+++ app.js
@@ -5,8 +5,25 @@ let app = new Koa();
 
 let port = process.argv[2];
 
-app.use(function (ctx, next) {
+app.use(async (ctx, next) => {
+    if (ctx.path !== "/") {
+        return await next()
+    }
     ctx.body = "hello koa";
+});
+
+app.use(async (ctx, next) => {
+    if (ctx.path !== "/404") {
+        return await next()
+    }
+    ctx.body = "page not found"
+});
+
+app.use(async (ctx, next) => {
+    if (ctx.path !== "/500") {
+        return await next();
+    }
+    ctx.body = "internal server error";
 });
 
 app.listen(port, function () {
