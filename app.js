2da9ab66e362b3631126059c4339302a380faa37
venkat <bvenkatr2@gmail.com>
2017-08-13 22:05:11 +0530

    done MIDDLEWARE

--- app.js
+++ app.js
@@ -6,12 +6,23 @@ let app = new Koa();
 let port = process.argv[2];
 
 app.use(async(ctx, next) => {
-    // I think we can use ctx.is('json') also
-    if (ctx.request.is('json')) {
-        ctx.body = {message: 'hi!'};
-    } else {
-        ctx.body = "ok";
-    }
+    //step 1
+    let startTime = Date.now();
+    await next();
+    //step 5
+    ctx.set("X-Response-Time", (Date.now() - startTime) / 60)
+});
+
+app.use(async(ctx, next) => {
+    //step 2
+    await next();
+    // step 4
+    ctx.body = ctx.body.toUpperCase();
+});
+
+app.use((ctx) => {
+    //step 3
+   ctx.body = "hello koa";
 });
 
 app.listen(port, function () {
