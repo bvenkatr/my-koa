679548378123b1464d6c17a9d163bf0d3f260b7d
venkat <bvenkatr2@gmail.com>
2017-08-13 20:35:59 +0530

    done CONTENT_HEADERS

--- app.js
+++ app.js
@@ -1,24 +1,17 @@
 let Koa = require("koa");
-let coBodyParser = require("co-body");
-let fs = require("fs");
 
 // get the Koa instance
 let app = new Koa();
 
 let port = process.argv[2];
 
-app.use(async (ctx, next) => {
-    if (ctx.path !== "/json") {
-        return await next();
+app.use(async(ctx, next) => {
+    // I think we can use ctx.is('json') also
+    if (ctx.request.is('json')) {
+        ctx.body = {message: 'hi!'};
+    } else {
+        ctx.body = "ok";
     }
-    ctx.body = {foo: 'bar'};
-});
-
-app.use(async (ctx, next) => {
-    if (ctx.path !== "/stream") {
-        return await next();
-    }
-    ctx.body = fs.createReadStream(process.argv[3]);
 });
 
 app.listen(port, function () {
