10e202b2d7e0d98371f81d3c3d5f7f2633820790
venkat <bvenkatr2@gmail.com>
2017-08-15 18:53:15 +0530

    done COOKIE

--- app.js
+++ app.js
@@ -5,21 +5,42 @@ let app = new Koa();
 
 let port = process.argv[2];
 
-app.use(async(ctx, next) => {
-   try {
-       await next();
-   } catch(error) {
-       ctx.status = 500;
-       ctx.body = error.message;
-   }
-});
+app.keys = ['secret', 'keys'];
 
-app.use((ctx) => {
-    if (ctx.path === "/error") throw new Error("internal server error");
-    ctx.status = 200;
-    ctx.body = "OK";
+app.use((ctx, next) => {
+    var n = ~~ctx.cookies.get("view", {signed: true}) + 1;
+    ctx.cookies.set("view", n, {signed: true});
+    ctx.body = n + " views";
 });
 
 app.listen(port, function () {
     console.log(`server is listening on ${port}`);
 });
+
+/*
+ koa uses the cookies module to operate cookies.
+
+ https://github.com/expressjs/cookies
+
+ APIs:
+
+ ctx.cookies.get(name, [options]): Get cookie name with options
+
+ * `signed`: the cookie requested should be signed
+
+ ctx.cookies.set(name, value, [options]): Set cookie name to value with options:
+
+ * `signed`: sign the cookie value
+ * `expires`: a Date for cookie expiration
+ * `path`: cookie path, '/' by default
+ * `domain`: cookie domain
+ * `secure`: secure cookie
+ * `httpOnly`: server-accessible cookie, true by default
+
+ Don't forget to set options.signed in get and set to make sure the cookie is signed.
+
+ And to use signed cookies, you need set app.keys:
+
+ var app = koa();
+ app.keys = ['secret', 'keys'];
+ */
