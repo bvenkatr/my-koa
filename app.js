a6aaa0afc980f168e6f042784e89cf0bce9c49b8
venkat <bvenkatr2@gmail.com>
2017-08-15 16:56:31 +0530

    done ERROR_HANDLING

--- app.js
+++ app.js
@@ -6,23 +6,16 @@ let app = new Koa();
 let port = process.argv[2];
 
 app.use(async(ctx, next) => {
-    //step 1
-    let startTime = Date.now();
-    await next();
-    //step 5
-    ctx.set("X-Response-Time", (Date.now() - startTime) / 60)
-});
-
-app.use(async(ctx, next) => {
-    //step 2
-    await next();
-    // step 4
-    ctx.body = ctx.body.toUpperCase();
+   try {
+       await next();
+   } catch(error) {
+       ctx.body = error.message;
+   }
 });
 
 app.use((ctx) => {
-    //step 3
-   ctx.body = "hello koa";
+    if (ctx.path === "/error") throw new Error("you are at /error path");
+    ctx.body = "sdlfkdl body";
 });
 
 app.listen(port, function () {
