0556e73eb35324e80d899a9dca380e769a5c47f2
venkat <bvenkatr2@gmail.com>
2017-08-15 17:51:18 +0530

    done ERROR_HANDLING

--- app.js
+++ app.js
@@ -9,13 +9,15 @@ app.use(async(ctx, next) => {
    try {
        await next();
    } catch(error) {
+       ctx.status = 500;
        ctx.body = error.message;
    }
 });
 
 app.use((ctx) => {
-    if (ctx.path === "/error") throw new Error("you are at /error path");
-    ctx.body = "sdlfkdl body";
+    if (ctx.path === "/error") throw new Error("internal server error");
+    ctx.status = 200;
+    ctx.body = "OK";
 });
 
 app.listen(port, function () {
